5f6628e10ed68276dae229982cc7cd22a4836fbc
wuami <mjwu@stanford.edu>
2015-11-13 08:18:40 -0800

    removed excess directories

--- benchmark/code/bin/RNAensign/config.py
+++
@@ -1,8 +0,0 @@
-# put the path to the RNAmutants directory here
-# To obtain RNAmutants, visit http://csb.cs.mcgill.ca/RNAmutants
-path_to_rna_mutants = '.'
-
-# We are including a copy of RNAfold with
-# the distribution, but if you would like to download your
-# own, put the path to the binary here
-path_to_rna_fold = '.' 
--- benchmark/code/parse_dss_out.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-with open(sys.argv[1]) as f:
-    lines = f.readlines()
-    target = lines[5].split()[2]
-    fold = lines[-2].split()[2]
-    if target == fold:
-        print lines[-1].split()[2]
