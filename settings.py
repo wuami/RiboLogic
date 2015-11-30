9c0a663f42e7993872eac371e9db63892599b002
wuami <mjwu@stanford.edu>
2015-11-30 15:27:35 -0800

    added osx nupack executables

--- settings.py
+++ settings.py
@@ -1,11 +1,13 @@
 import os
 
 file_path = os.path.realpath(__file__)
-spl = file_path.split("/")
-base_dir = "/".join(spl[:-1])
+spl = file_path.split('/')
+base_dir = '/'.join(spl[:-1])
 
-RESOURCE_DIR = os.path.join(base_dir, "resources")
-STRATEGY_DIR = os.path.join(base_dir, "strategies")
-VIENNA_DIR = os.path.join(RESOURCE_DIR, "vienna", "linux")
-NUPACK_DIR = os.path.join(RESOURCE_DIR, "nupack")
+os = 'linux'
+
+RESOURCE_DIR = os.path.join(base_dir, 'resources')
+STRATEGY_DIR = os.path.join(base_dir, 'strategies')
+VIENNA_DIR = os.path.join(RESOURCE_DIR, 'vienna', os)
+NUPACK_DIR = os.path.join(RESOURCE_DIR, 'nupack', os)
 
