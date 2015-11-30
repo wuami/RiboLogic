f4b678e94df4a22da5ec8904c9a9cfbb9103256f
wuami <mjwu@stanford.edu>
2015-11-30 15:37:56 -0800

    automatic detection of os

--- settings.py
+++ settings.py
@@ -1,13 +1,20 @@
-import os
+import os, sys, platform
 
 file_path = os.path.realpath(__file__)
 spl = file_path.split('/')
 base_dir = '/'.join(spl[:-1])
 
-os = 'linux'
+pf = platform.system()
+if pf == 'Linux':
+    os_dir = 'linux'
+elif pf == 'Darwin':
+    os_dir = 'osx'
+else:
+    print "%s platform not supported"
+    sys.exit()
 
 RESOURCE_DIR = os.path.join(base_dir, 'resources')
 STRATEGY_DIR = os.path.join(base_dir, 'strategies')
-VIENNA_DIR = os.path.join(RESOURCE_DIR, 'vienna', os)
-NUPACK_DIR = os.path.join(RESOURCE_DIR, 'nupack', os)
+VIENNA_DIR = os.path.join(RESOURCE_DIR, 'vienna', os_dir)
+NUPACK_DIR = os.path.join(RESOURCE_DIR, 'nupack', os_dir)
 
