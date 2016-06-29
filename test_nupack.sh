cdbfdc2b71cf01cdbc94abb9f84c81e1de44bd07
mkapl <mtskpln@gmail.com>
2016-06-29 11:48:28 -0700

    changed file directory

--- test_nupack.sh
+++ test_nupack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PBS -M mjwu@stanford.edu
+#PBS -M mkap@stanford.edu
 #PBS -m ea
 #PBS -l walltime=2:00:00:00
 #PBS -l nodes=1:ppn=4
@@ -11,4 +11,4 @@ niter=100000
 conc="1"
 echo ${puzzle}", $niter iter, start conc $conc, BPP SCORING"
 echo ${options}
-python2.7 /home/mjwu/EteRNABot/eternabot/design_sequence.py -m nupack --print_ -i $niter -c $conc ${options} ${puzzle}
+python2.7 /Users/Matias/Box\ Sync/2016\ Summer/Das_Rotation/RiboLogic/design_sequence.py -m nupack --print_ -i $niter -c $conc ${options} ${puzzle}
