f2befcff7ff961a26423fe036ae68fb8bca312e9
[Matias Kaplan] <[mkap@stanford.edu]>
2016-07-13 17:30:31 -0700

    fixed

--- randomer.py
+++ randomer.py
@@ -4,15 +4,15 @@ import string
 import numpy as np
 import settings
 
-
-
 nucleotides = ['A','C','G','U']
 def randomer(size):
     return ''.join(np.random.choice(nucleotides) for _ in range(size))
 
 n_targets = 1
-sequence = randomer(20)
+sequence = randomer(20) + '&' + randomer(20)
 p = multiprocessing.Pool(n_targets)
 result = [None] * n_targets
 
-fold_list = fold_utils.vienna_fold(sequence, bpp=True)
+result[0]  = fold_utils.vienna_fold(sequence, bpp=True)
+print result[0][0]
+print sequence
